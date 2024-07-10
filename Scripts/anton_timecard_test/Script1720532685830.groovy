e01da8e94371580f89bdb9e35b04b894694cedaa
Anton Zimin <antonz@appdome.com>
2024-07-10 16:36:20 +0300

    waits instead of finding element

--- Scripts/anton_timecard_test/Script1720532685830.groovy
+++ Scripts/anton_timecard_test/Script1720532685830.groovy
@@ -3,6 +3,19 @@ import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.util.internal.PathUtil as PathUtil
 import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 
 Mobile.comment('Story: Verify correct alarm message')
 
@@ -13,7 +26,9 @@ def appPath = PathUtil.relativeToAbsolutePath(GlobalVariable.G_AppPath, RunConfi
 
 Mobile.startApplication(appPath, false)
 
-Mobile.verifyElementExist(findTestObject('Object Repository/android.widget.ImageButton'), 120)
+not_run: Mobile.verifyElementExist(findTestObject('Object Repository/android.widget.ImageButton'), 120)
+
+WebUI.delay(20)
 
 Mobile.closeApplication()
 
